fbb983719159b1c1da57115bbae5b59459d68645
zyumbik <gleb.sabirzyanov@gmail.com>
2018-08-12 13:39:14 +1000

    Initial Commit

---
+++ Awake/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Awake
+//
+//  Created by Gleb Sabirzyanov on 12/08/2018.
+//  Copyright © 2018 Gleb Sabirzyanov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Awake/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Awake
+//
+//  Created by Gleb Sabirzyanov on 12/08/2018.
+//  Copyright © 2018 Gleb Sabirzyanov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
